abfc1f489c930b9f842fb60ed1a1b7473231e373
Masis <masiskar@gmail.com>
2020-12-22 20:48:35 +0400

    fixed logger

--- src/store/store.js
+++ src/store/store.js
@@ -5,12 +5,12 @@ import logger from 'redux-logger';
 import {mainReducer} from './reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-
+const middlewaresArr = [thunk];
 if(process.env.NODE_ENV === "development"){
-
+    middlewaresArr.push(logger);
 }
 
-const middlewares = applyMiddleware(thunk, logger);
+const middlewares = applyMiddleware(...middlewaresArr);
 
 export const store = createStore(mainReducer, composeWithDevTools(middlewares));
 
